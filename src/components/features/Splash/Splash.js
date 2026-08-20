3062cf735e2888d23b2465bb130f102dcd603f7a
N1Alien <n1anoobis@gmail.com>
2026-08-20 17:22:30 +0200

    Fix: Replace broken react-particles with smooth pure CSS starfield animation

--- src/components/features/Splash/Splash.js
+++ src/components/features/Splash/Splash.js
@@ -2,20 +2,10 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import styles from './Splash.module.scss';
-import 'particles.js';
 import { connect } from 'react-redux';
 import { loadCars } from '../../../redux/carRedux';
 
 const Component = ({ className, getCars }) => {
-
-  useEffect(() => {
-    if (window.particlesJS) {
-      window.particlesJS('particles-js', {
-        /* Global configuration fallback */
-      });
-    }
-  }, []);
-
   const [flag, setFlag] = useState(false);
 
   useEffect(() => {
@@ -32,80 +22,33 @@ const Component = ({ className, getCars }) => {
   };
 
   return (
-    <div className={clsx(className, styles.root)} onClick={hideSplash} onKeyDown={hideSplash} role="button" tabIndex={0}>
+    <div 
+      className={clsx(className, styles.root)} 
+      onClick={hideSplash} 
+      onKeyDown={hideSplash} 
+      role="button" 
+      tabIndex={0}
+    >
       <div className={flag ? styles.hide : styles.show}>
-        <div 
-          id="particles-js" 
-          style={{ position: 'absolute', width: '100%', height: '100%' }} 
-          canvasClassName={styles.part}
-          params={{
-            'particles': {
-              'number': {
-                'value': 80,
-                'density': {
-                  'enable': true,
-                  'value_area': 800,
-                },
-              },
-              'color': {
-                'value': '#ffffff',
-              },
-              'shape': {
-                'type': 'circle',
-              },
-              'opacity': {
-                'value': 0.5,
-                'random': false,
-              },
-              'size': {
-                'value': 3,
-                'random': true,
-              },
-              'line_linked': {
-                'enable': true,
-                'distance': 150,
-                'color': '#ffffff',
-                'opacity': 0.4,
-                'width': 1,
-              },
-              'move': {
-                'enable': true,
-                'speed': 2,
-                'direction': 'none',
-                'random': false,
-                'straight': false,
-                'out_mode': 'out',
-              },
-            },
-            'interactivity': {
-              'detect_on': 'canvas',
-              'events': {
-                'onhover': {
-                  'enable': true,
-                  'mode': 'grab',
-                },
-                'onclick': {
-                  'enable': true,
-                  'mode': 'push',
-                },
-                'resize': true,
-              },
-            },
-            'retina_detect': true,
-          }}
-        />
+        {/* Nowoczesne, czyste tło CSS zastępujące wadliwy skrypt particles.js */}
+        <div className={styles.starsContainer}>
+          <div className={styles.stars}></div>
+          <div className={styles.stars2}></div>
+          <div className={styles.stars3}></div>
+        </div>
+        
         <div className={styles.header}>
           3D-Printed Cars
         </div>
       </div>
-    </div >
+    </div>
   );
 };
 
 Component.propTypes = {
   className: PropTypes.string,
   getCars: PropTypes.func,
-};
+ };
 
 const mapDispatchToProps = dispatch => ({
   getCars: () => dispatch(loadCars()),
